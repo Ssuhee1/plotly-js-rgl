af2c16166889099a1887443a07ce82b7d25a794b
Sukhbat <atmaanboljmor@gmail.com>
2022-08-30 20:45:23 +0800

    suhe

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,12 @@
-// todo
-
-// react flow   [O]
-// table        [O]
-// comment      [?]
-// grafana      [?]
-
-import { useState } from "react";
-
-import ReactFlowTest from "../components/reactflow";
-import TableTest from "../components/table";
-import Comment from "../components/comment";
-import Test from "../components/mysql";
-import dynamic from "next/dynamic";
-const Graph = dynamic(() => import("../components/graph"), { ssr: false });
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const Home = () => {
-  const [tab, setTab] = useState("test");
-
-  return (
-    <>
-      <div>
-        <button
-          onClick={() => setTab("test")}
-          className="btn btn-outline-secondary me-3"
-        >
-          Test
-        </button>
-        <button
-          onClick={() => setTab("dulguun")}
-          className="btn btn-outline-secondary"
-        >
-          Dulguun ah
-        </button>
-      </div>
-      {tab === "dulguun" && (
-        <>
-          <ReactFlowTest />
-          <TableTest />
-          <Comment />
-        </>
-      )}
-
-      {tab === "test" && (
-        <>
-          {/* <Test /> */}
-          <Graph />
-        </>
-      )}
-    </>
-  );
+  const router = useRouter();
+  const changeLocationToRglPage = () => router.push("/rgl");
+  useEffect(() => {
+    changeLocationToRglPage();
+  }, []);
+  return <></>;
 };
 export default Home;
